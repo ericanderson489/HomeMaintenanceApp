ebde8d3791c478aed31dfec373ab59ef39146a62
FuriousTitan29 <jack.delea@gmail.com>
2026-09-17 09:20:12 -0400

    Added account class and file reading class used for reading text files to reconstruct classes.

---
+++ FileReading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeMaintenanceApp
+{
+	internal class FileReading
+	{
+		public string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SavedAccounts.txt");
+
+		public List<string> accountsInFile = new List<string>();
+
+		public FileReading()
+		{
+			accountsInFile = File.ReadAllLines(path).ToList();
+		}
+
+		public void FullRewrite(List<Account> accounts) //Rewrites the file containing all saved accounts 
+		{
+			List<string> lines = new List<string>();
+
+			foreach (Account account in accounts)
+			{
+				lines.Add(account.GiveFileString());
+			}
+
+			File.WriteAllLines(path, lines);
+		}
+	}
+}
