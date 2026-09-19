cb38d1d655d863ff4974d7c13aac9b1cd77865dc
KWH12 <kwhsoccer@gmail.com>
2026-09-19 13:56:53 -0400

    Profile and Tasks class WIP
    Profile stores user info/tasks, Tasks handles creation of tasks.

---
+++ Profile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeMaintenanceApp
+{
+    internal class Profile
+    {
+        private string m_username;
+        private string m_password;
+        private int m_taskCount = 0;
+        private List<Tasks> m_taskList;
+
+        // Could be useful to store unique IDs for users?
+        // private Guid m_ID;
+
+        public Profile (string username, string password)
+        {
+            m_username = username;
+            m_password = password;
+            m_taskList = new List<Tasks>();
+        }
+
+        // Get and Set methods for Profile variables
+
+        public int GetTaskCount() { return m_taskCount; }
+        public void SetTaskCount(int taskCount) { m_taskCount = taskCount; }
+
+        public List<Tasks> GetTaskList() { return m_taskList; }
+        public void SetTaskList(List<Tasks> taskList) {  m_taskList = taskList; }
+
+        // Add Tasks. Does it need to check against what is already there to prevent duplicates?
+        public void AddTask(string name, string description, string type, DateTime time)
+        {
+            Tasks newTask = new Tasks(name, description, type, time);
+            m_taskList.Add(newTask);
+        }
+
+        // Delete Tasks. Returns bool so if task not found, prompts user that task is not in list.
+        public bool DeleteTask(string taskName)
+        {
+            bool isFound = false;
+
+            foreach (var task in GetTaskList())
+            {
+                if (taskName.ToLower().TrimEnd() == task.GetName().ToLower().TrimEnd())
+                {
+                    GetTaskList().Remove(task);
+                    isFound = true;
+                    return isFound;
+                }
+            }
+
+            return isFound;
+        }
+
+        // Edit Tasks. Returns bool so if task not found, prompts user that task is not in list.
+        public bool EditTask(string name, string description, string type, DateTime time)
+        {
+            bool isFound = false;
+
+            foreach (var task in GetTaskList())
+            {
+                if (name.ToLower().TrimEnd() == task.GetName().ToLower().TrimEnd())
+                {
+                    GetTaskList().Remove(task);
+                    GetTaskList().Add(new Tasks(name, description, type, time));
+                    isFound = true;
+                    return isFound;
+                }
+            }
+
+            return isFound;
+        }
+    }
+}
---
+++ Tasks.cs
@@ -0,0 +1,64 @@
+﻿/* Task Class
+ * Kiefer
+ * Enables creation of tasks, helper functions, etc.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeMaintenanceApp
+{
+    // Status of task. Is it pending, in progress or done?
+    public enum Status
+    {
+        Pending,
+        WIP,
+        Complete
+    }
+
+    // Frequency for recurring tasks
+    public enum Frequency
+    {
+        Weekly,
+        Monthly,
+        Quarterly,
+        Annually,
+    }
+    internal class Tasks
+    {
+        private string m_name;
+        private string m_description;
+        private string m_type;
+        // To be implemented in a future build when UI is established
+        // private Frequency m_frequency;
+        // private Status m_status;
+        private DateTime m_dueDate;
+
+        public Tasks (string name, string description, string type, DateTime dueDate)
+        {
+            m_name = name;
+            m_description = description;
+            m_type = type;
+            m_dueDate = dueDate;
+        }
+
+        // Set and Get methods for Tasks objects
+        public string GetName() { return m_name; }
+        public void SetName(string name) { m_name = name; }
+
+        public string GetDescription() { return m_description; }
+        public void SetDescription(string description) { m_description = description; }
+
+        public string GetTaskType() { return m_type; }
+        public void SetType(string type) { m_type = type; }
+
+        public Frequency GetFrequency() { return m_frequency; }
+        public void SetFrequency(Frequency frequency) { m_frequency = frequency; }
+
+        public Status GetStatus() { return m_status; }
+        public void SetStatus(Status status) { m_status = status; }
+    }
+}
