0a7294955b3359db879793b3b54192fa57d2abef
eandy63 <wh0lyself@gmail.com>
2026-09-21 10:32:29 -0400

    updated goals page

--- GoalsControl.Designer.cs
+++ GoalsControl.Designer.cs
@@ -38,7 +38,7 @@
             addGoalsButton.FlatStyle = FlatStyle.Popup;
             addGoalsButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             addGoalsButton.ForeColor = SystemColors.ButtonHighlight;
-            addGoalsButton.Location = new Point(326, 1);
+            addGoalsButton.Location = new Point(561, 0);
             addGoalsButton.Name = "addGoalsButton";
             addGoalsButton.Size = new Size(107, 40);
             addGoalsButton.TabIndex = 0;
