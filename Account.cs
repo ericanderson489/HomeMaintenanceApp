25a5ef015ea7aa502e47ecff1558851fe3c6ba2d
eric anderson <wh0lyself@gmail.com>
2026-09-22 23:36:40 -0400

    updates to account and added tasks file. updates are pulled from KieferBranch

--- Account.cs
+++ Account.cs
@@ -12,12 +12,14 @@ namespace HomeMaintenanceApp
         //List<Tasks> tasks = new List<Tasks>(); //Just in case we have a list of tasks tied to each account
         private string m_firstName, m_lastName, m_userName;
         private string m_password = "";
+        private List<Tasks> m_taskList;
         public Account(string username, string password, string firstName, string lastName) // Constructor for the account
         {
             m_userName = username;
             m_firstName = firstName;
             m_lastName = lastName;
             m_password = password;
+            m_taskList = new List<Tasks>();
         }
         public void CopyFrom(Account account) // Copies information from one account to another.
         {
@@ -50,6 +52,63 @@ namespace HomeMaintenanceApp
         public string GiveFileString() //Returns a string that is used in file-rewriting - JD
         {
             return ($"{m_userName} {m_password} {m_firstName} {m_lastName}");
+        }
+        // Returns all tasks in the account
+        public List<Tasks> GetTaskList()
+        {
+            return m_taskList;
+        }
+        // Returns total number of tasks tied to account
+        public int GetTaskCount()
+        {
+            return m_taskList.Count;
+        }
+        // Creates new task and adds it to the account
+        public void AddTask(string name, string description, string type, DateTime time)
+        {
+            Tasks newTask = new Tasks(name, description, type, time);
+            m_taskList.Add(newTask);
+        }
+        // Adds task object
+        public void AddTask(Tasks newTask)
+        {
+            m_taskList.Add(newTask);
+        }
+        // Deletes specific task
+        public bool DeleteTask(string taskName)
+        {
+            for (int i = 0; i < m_taskList.Count; i++)
+            {
+                if (m_taskList[i].GetName().Equals(
+                    taskName.Trim(),
+                    StringComparison.OrdinalIgnoreCase))
+                {
+                    m_taskList.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        // Finds existing task and updates it
+        public bool EditTask(
+            string name,
+            string description,
+            string type,
+            DateTime time)
+        {
+            foreach (Tasks task in m_taskList)
+            {
+                if (task.GetName().Equals(
+                    name.Trim(),
+                    StringComparison.OrdinalIgnoreCase))
+                {
+                    task.SetDescription(description);
+                    task.SetType(type);
+                    task.SetDate(time);
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
---
+++ Tasks.cs
@@ -0,0 +1,41 @@
+﻿namespace HomeMaintenanceApp
+{
+    internal enum Status
+    {
+        Pending,
+        Complete
+    }
+    internal enum Frequency
+    {
+        Weekly,
+        Monthly,
+        Quarterly,
+        Annually
+    }
+    internal class Tasks
+    {
+        private string m_name;
+        private string m_description;
+        private string m_type;
+        private Status m_status;
+        private DateTime m_dueDate;
+        public Tasks(string name, string description, string type, DateTime dueDate)
+        {
+            m_name = name;
+            m_description = description;
+            m_type = type;
+            m_dueDate = dueDate;
+            m_status = Status.Pending;
+        }
+        public string GetName() { return m_name; }
+        public void SetName(string name) { m_name = name; }
+        public string GetDescription() { return m_description; }
+        public void SetDescription(string description) { m_description = description; }
+        public string GetTaskType() { return m_type; }
+        public void SetType(string type) { m_type = type; }
+        public DateTime GetDate() { return m_dueDate; }
+        public void SetDate(DateTime date) { m_dueDate = date; }
+        public Status GetStatus() { return m_status; }
+        public void SetStatus(Status status) { m_status = status; }
+    }
+}
